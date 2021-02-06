375c52e37658ec604a64d3c43b9510c4aa3ae705
AlfredoCU <alfredo_11_cu@hotmail.com>
2021-02-06 17:17:19 -0600

    Se agrego un uppercase al texto del botón.

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,7 @@ const Button = ({ title, type }) => (
   <div className="p-2">
     <button
       type={type}
-      className="transition ease-in duration-500 hover:bg-yellow-500 w-full px-5 py-2 bg-blue-500 rounded font-bold focus:outline-white"
+      className="transition ease-in duration-500 hover:bg-yellow-500 w-full px-5 py-2 bg-blue-500 rounded uppercase font-bold focus:outline-white"
     >
       {title}
     </button>

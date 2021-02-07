07bc5da6af77ed28ff571e48589926b11909334f
AlfredoCU <alfredo_11_cu@hotmail.com>
2021-02-07 00:56:25 -0600

    NEW BUILD.

--- src/components/Img.js
+++ src/components/Img.js
@@ -1,9 +1,11 @@
-import crypto from "../assets/crypto.png";
+import { memo } from "react";
+import { IcCrypto } from "../assets/IcCrypto";
 
 const Img = () => (
   <div className="p-5 lg:p-10 order-last sm:order-first">
-    <img src={crypto} alt="Imagen de Criptomonedas" />
+    <IcCrypto />
   </div>
 );
 
-export { Img };
+const ImgMemo = memo(Img);
+export { ImgMemo as Img };
